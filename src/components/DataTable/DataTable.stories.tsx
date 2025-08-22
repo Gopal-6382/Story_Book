11af7ae8c947fb110d68b44a10f4bab2518c4152
Gopal-6282 <gopalkrishnanto@email.com>
2025-08-22 13:45:06 +0530

    webpack

--- src/components/DataTable/DataTable.stories.tsx
+++ src/components/DataTable/DataTable.stories.tsx
@@ -1,4 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
+
 import DataTable from "./DataTable";
 import type { DataTableProps, Column } from "../../types";
 
